b6d8fee8933dab7d5d0f4680fb78d24985bb1b15
Karthik Kothuri <kkothuri@microsoft.com>
2023-07-26 15:42:08 +0530

    init matchiterator

--- RegexParser/MatchIterator.cpp
+++ RegexParser/MatchIterator.cpp
@@ -1,2 +1,31 @@
 #include "MatchIterator.h"
 
+MatchIterator::MatchIterator(std::shared_ptr<ParseTree> root, int start)
+{
+	pathStack.push(root);
+	iterStack.push(0);
+	startIndex = start;
+	match = false;
+	dead = false;
+	iterationCounter = 0;
+}
+
+std::vector<MatchIterator> MatchIterator::consume(wchar_t input)
+{
+
+}
+
+int MatchIterator::getStartIndex()
+{
+	return startIndex;
+}
+
+bool MatchIterator::isMatch()
+{
+	return match;
+}
+
+bool MatchIterator::isDead()
+{
+	return dead;
+}
--- RegexParser/MatchIterator.h
+++ RegexParser/MatchIterator.h
@@ -6,12 +6,14 @@
 class MatchIterator
 {
 	std::stack<std::shared_ptr<ParseTree>> pathStack;
+	std::stack<int> iterStack;
 	int startIndex;
 	bool match;
 	bool dead;
 	int iterationCounter;
 	
 public:
+	MatchIterator(std::shared_ptr<ParseTree> root, int start);
 	std::vector<MatchIterator> consume(wchar_t input);
 	int getStartIndex();
 	bool isMatch();
